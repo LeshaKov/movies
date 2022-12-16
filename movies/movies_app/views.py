29f4bc44747db456cf91c930e9815aa43e2b6bca
LeshaKov <lelya.kovalevskiy@gmail.com>
2022-12-17 02:38:24 +0300

    cadres and reviews

--- movies/movies_app/views.py
+++ movies/movies_app/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import redirect
+from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 from movies_app.models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -12,3 +15,14 @@ class MovieDetailView(DetailView):
     model = Movie
     slug_field = "url"
     template_name = 'movies_app/movie.html'
+
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())

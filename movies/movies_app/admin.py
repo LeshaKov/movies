514366f52b72ff09d11e9950a65f79f44e1122fb
LeshaKov <lelya.kovalevskiy@gmail.com>
2022-12-17 05:11:51 +0300

    update admin.py

--- movies/movies_app/admin.py
+++ movies/movies_app/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
-admin.site.register(ActorDirector)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieCadres)
-admin.site.register(Reviews)
 
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("id", "name", "year", "country", "rating", "url", "is_published", "category")
+    search_fields = ("name", "category__name")
+    list_editable = ("is_published",)
+    list_filter = ("category", "year", "rating", "genres")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("id", "name", "url")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+@admin.register(MovieCadres)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ("id", "title", "movie", "image")
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("id", "name", "email", "parent", "movie")
+    readonly_fields = ("name", "email")
+
+
+@admin.register(ActorDirector)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("id", "name", "age", "image")
--- movies/movies_app/apps.py
+++ movies/movies_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MoviesAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies_app'
+    verbose_name = "Фильмы"
